addaaba854181e5e7723d540b5c02cb7ed1fe1a4
nbkhope <ryumemaru@yahoo.com>
2017-07-15 17:55:34 -0700

    Fix issue with duplicate error property

--- PuppyPlayDate/src/reducers/reducer_auth.js
+++ PuppyPlayDate/src/reducers/reducer_auth.js
@@ -14,16 +14,18 @@ const INITIAL_STATE = {
   user: null // right now user actually contains userId
 };
 
-export default function(state = INITIAL_STATE, action) {
-  switch(action.type) {
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
     case AUTH_USER:
       console.log("AUTH_USER!!!");
       return { ...state, authenticated: true, user: action.payload, password: '', error: '' };
     case UNAUTH_USER:
       return { ...state, authenticated: false, user: null, error: '' };
-    case AUTH_ERROR:
+    case AUTH_ERROR: {
       console.log("AUTH_ERROR!!");
-      return { ...state, error: action.payload, error: 'Authentication failed.', password: '' };
+      const error = action.payload || 'Authentication failed.';
+      return { ...state, error, password: '' };
+    }
     case CHANGE_USERNAME:
       return { ...state, username: action.payload };
     case CHANGE_PASSWORD:
